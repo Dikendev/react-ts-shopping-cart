6e61ec31d9a999a8ccc6f814509b8bccaea137ba
Dikendev <diken.dev@gmail.com>
2023-07-16 21:30:05 -0300

    store items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,13 @@
 import { Col, Row } from "react-bootstrap";
 import storeItem from "../data/item.json"
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return <><h1>Store</h1>
   <Row md={1} xs={1} lg={2} className="g-3">
     {storeItem.map(item => (
-      <Col>
-        {JSON.stringify(item)}
+      <Col key={item.id}>
+        <StoreItem {...item} />
       </Col>
     ))}
     <Col></Col>
